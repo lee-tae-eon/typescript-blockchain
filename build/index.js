4dba4922fd790204d9dca51982a2dcdbbf66d17f
이태언 <mktears0609@gmail.com>
2022-10-25 06:12:31 +0900

    ts config

--- build/index.js
+++ build/index.js
@@ -1,1 +1,9 @@
-var hello = function () { return "hi"; };
+var Block = /** @class */ (function () {
+    function Block(data) {
+        this.data = data;
+    }
+    Block.hello = function () {
+        return "hi";
+    };
+    return Block;
+}());
