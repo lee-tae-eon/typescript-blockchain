25652ef1c2f04f31cdb2a4a6ecf8c390f20d76c0
이태언 <mktears0609@gmail.com>
2022-10-30 21:18:40 +0900

    allowjs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-import { init, exit } from "myPackage";
-
-init({ urls: "test" });
-exit({ test: 3 });
-class Block {
-  constructor(private data: string) {}
-  static hello() {
-    return "hi";
-  }
-}
+import { init, exit } from "./myPackage";
