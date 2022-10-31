3f76b2416938efb3a34e33351283f3993c61ff25
이태언 <mktears0609@gmail.com>
2022-10-31 22:58:04 +0900

    FIX: script 수정

---
+++ src/test.ts
@@ -0,0 +1,7 @@
+// * typescript 는 데이터와 변수의 타입을 명시적으로 정의할 수도 있고
+// * javscript처럼 변수만 생성해도 됨 - typescript 가 타입 추론을 해줌
+
+let a = "hello"; //* implicit type
+let b: boolean = false; //* explicit type
+
+console.log(a);
