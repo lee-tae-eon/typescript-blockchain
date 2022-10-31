53faf33ceb676643baad2dc567796cd734350b45
이태언 <mktears0609@gmail.com>
2022-10-31 23:12:41 +0900

    implicit explicit object

--- src/test.ts
+++ src/test.ts
@@ -3,5 +3,27 @@
 
 let a = "hello"; //* implicit type
 let b: boolean = false; //* explicit type
+let c = [1, 2, 3];
 
-console.log(a);
+// * 명시적 타입은 최소한으로 쓰는 게 좋다. 타입스크립트가 타입 추론을 하도록 하는게 나음
+
+type Age = number;
+
+type Player = {
+  name: string;
+  age?: Age;
+};
+
+const player: Player = {
+  name: "devlee",
+  age: 3,
+};
+const playLynn: Player = {
+  name: "Lynn",
+};
+
+function createPlayer(name: string, age?: number): Player {
+  return {
+    name,
+  };
+}
