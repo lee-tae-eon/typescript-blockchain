54d9c1fab43d57dc748cb22de8c789683b005caf
이태언 <mktears0609@gmail.com>
2022-11-14 15:24:59 +0900

    ADD: overloading call signature

--- src/callSignatures.ts
+++ src/callSignatures.ts
@@ -3,7 +3,11 @@ function add(a: number, b: number) {
 }
 // * function call signature
 type Add2Type = (a: number, b: number) => number;
+// * 다른 표기 법 - overloading을 위해
+type Add3Type = { (a: number, b: number): number };
 
 const add2: Add2Type = (a, b) => {
   return a + b;
 };
+
+// * overloading은 함수가 여러개의 call signature를 가지고 있을때 발생
