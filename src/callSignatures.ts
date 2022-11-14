50e1be4250b86e77d6bf75ea7e695bdcccd3b488
이태언 <mktears0609@gmail.com>
2022-11-14 15:28:35 +0900

    type check overloading

--- src/callSignatures.ts
+++ src/callSignatures.ts
@@ -3,11 +3,18 @@ function add(a: number, b: number) {
 }
 // * function call signature
 type Add2Type = (a: number, b: number) => number;
-// * 다른 표기 법 - overloading을 위해
-type Add3Type = { (a: number, b: number): number };
 
 const add2: Add2Type = (a, b) => {
   return a + b;
 };
 
 // * overloading은 함수가 여러개의 call signature를 가지고 있을때 발생
+type Add3Type = {
+  (a: number, b: number): number;
+  (a: number, b: string): number;
+};
+// * - overloading이 일어난 후 b 는 number or string이 되기 때문에 error 발생 그래서 type check
+const add3: Add3Type = (a, b) => {
+  if (typeof b === "string") return a;
+  return a + b;
+};
