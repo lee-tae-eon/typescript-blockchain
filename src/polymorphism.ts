880732e5f8cba6b7ec45c52edc413b57129752ea
이태언 <mktears0609@gmail.com>
2022-11-27 21:29:25 +0900

    polymorphism

---
+++ src/polymorphism.ts
@@ -0,0 +1,17 @@
+// * polymorphism - 다형성
+
+// * call signature를 통해 여러개를 주는 것도 방법이지만
+// * polymorphism을 이용해 generic을 주는 것이 더 좋다.
+type SuperPrint = {
+  (arr: number[]): void;
+  (arr: boolean[]): void;
+  (arr: string[]): void;
+};
+
+const superPrint: SuperPrint = (arr) => {
+  arr.forEach((i) => console.log(i));
+};
+
+superPrint([1, 2, 3, 4]);
+superPrint([true, false, false, true]);
+superPrint(["1", "@", "3"]);
