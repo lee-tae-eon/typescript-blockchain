80ad5ad706ff2131d5c62e9b717236ac2a0daeb3
이태언 <mktears0609@gmail.com>
2022-11-27 21:32:30 +0900

    Generic

--- src/polymorphism.ts
+++ src/polymorphism.ts
@@ -3,9 +3,10 @@
 // * call signature를 통해 여러개를 주는 것도 방법이지만
 // * polymorphism을 이용해 generic을 주는 것이 더 좋다.
 type SuperPrint = {
-  (arr: number[]): void;
-  (arr: boolean[]): void;
-  (arr: string[]): void;
+  // * generic 사용법
+  <T>(arr: T[]): void;
+  // (arr: boolean[]): void;
+  // (arr: string[]): void;
 };
 
 const superPrint: SuperPrint = (arr) => {
@@ -15,3 +16,4 @@ const superPrint: SuperPrint = (arr) => {
 superPrint([1, 2, 3, 4]);
 superPrint([true, false, false, true]);
 superPrint(["1", "@", "3"]);
+superPrint(["1", "@", "3", true, false]);
