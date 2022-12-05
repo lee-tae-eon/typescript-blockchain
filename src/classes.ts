6b4cc3dfcf3e28be23dee22f4a274eae6a0a6e2b
이태언 <mktears0609@gmail.com>
2022-12-05 20:02:13 +0900

    TEST: abstract class

--- src/classes.ts
+++ src/classes.ts
@@ -1,9 +1,15 @@
+// * abstract class = 추상 클래스는 다른 클래스가 상속받을 수 있는 클래스이다.
+// * abstract class didn't create instance
+// * 상속만 가능한 클래스이다.
 abstract class User {
   constructor(
     private firstName: string,
     private lastName: string,
     public nickName: string
   ) {}
+  private getFullName() {
+    return `${this.firstName}-${this.lastName}`;
+  }
 }
 
 class Player extends User {}
