3b33a47feeaecf97f07ca2a0dc615756d7d81c5b
이태언 <mktears0609@gmail.com>
2022-12-05 20:09:42 +0900

    TEST: abstract protected

--- src/classes.ts
+++ src/classes.ts
@@ -5,16 +5,30 @@ abstract class User {
   constructor(
     private firstName: string,
     private lastName: string,
-    public nickName: string
+    public nickName: string,
+    protected wifeName?: string
   ) {}
+
+  // * abstract methods = 추상 클래스를 상속받는 클래스들이 구현해야하는 메소드이다.
+  // * property를 private으로 만들면 클래스를 상속하더라도 그 property 에 접근할수 없다
+  abstract getNickName(): void;
+
   private getFullName() {
     return `${this.firstName}-${this.lastName}`;
   }
 }
 
-class Player extends User {}
+class Player extends User {
+  getNickName(): void {
+    // * property를 private으로 만들면 클래스를 상속하더라도 그 property 에 접근할수 없다
+    // * 외부로부터는 보호되지만 자식 클래스에서 사용도되기를 원하면 protected를 써야한다.
+    console.log(`${this.wifeName}-${this.nickName}`);
+  }
+}
 
-const taeeon = new Player("lee", "taeeon", "taeeon");
+const taeeon = new Player("lee", "taeeon", "taeeon", "huikyung");
+
+taeeon.getNickName();
 
 // * in javascript
 // * class Player {
