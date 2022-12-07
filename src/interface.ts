764415cd784292657a0589c7220a58909edf9767
이태언 <mktears0609@gmail.com>
2022-12-08 06:19:38 +0900

    TEST: interface

--- src/interface.ts
+++ src/interface.ts
@@ -1,12 +1,23 @@
 type Nick = "test" | "lee" | "eon";
 type Health = 1 | 5 | 10;
 
-type Standards = {
+// * interface 는 object의 shape을 결정해주려는 역할로 만들어진 type 정의이다.
+interface Standards {
   nickname: Nick;
   healthBar: Health;
-};
+}
 
 const test: Standards = {
   nickname: "test",
   healthBar: 1,
+};
+
+interface SuperUser {
+  name: string;
+}
+
+interface HealthPlayer extends SuperUser {}
+
+const option: HealthPlayer = {
+  name: "taeeon",
 };
