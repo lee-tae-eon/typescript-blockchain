956f178c503c13051e0ebf3005b467b816ebd250
이태언 <mktears0609@gmail.com>
2022-12-08 06:13:27 +0900

    TEST: interface

--- src/interface.ts
+++ src/interface.ts
@@ -1,9 +1,12 @@
+type Nick = "test" | "lee" | "eon";
+type Health = 1 | 5 | 10;
+
 type Standards = {
-  nickname: string;
-  healthBar: number;
+  nickname: Nick;
+  healthBar: Health;
 };
 
 const test: Standards = {
   nickname: "test",
-  healthBar: 32,
+  healthBar: 1,
 };
