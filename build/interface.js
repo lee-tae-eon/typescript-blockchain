c5881670e78bf90de6c76b3d2cdc0b61abafe445
이태언 <mktears0609@gmail.com>
2023-01-16 22:35:58 +0900

    TEST: polimorphism & interface & generic

--- build/interface.js
+++ build/interface.js
@@ -6,19 +6,37 @@ const test = {
 const option = {
     name: "taeeon",
 };
-// * ----------------------------------
-console.log("test");
-class IUser {
-    constructor(firstName, lastName) {
+// interface를 class에서 extends할때는 typescript에서 제공하는 implements를 사용하자.
+// interface를 상속할 때에는 property를 private, protected 으로 만들지 못한다.
+// file size를 줄이는데 큰 역할을 할수있다.
+// 여러개의 interface를 동시에 상속이 가능하다.
+// 동시에 여러 곳에서 사용가능 함수의 파라미터 타입도 되고 아무튼 여러 곳에서 사용가능하다는 장점이 있다.
+class IPlayer {
+    constructor(firstName, lastName, health) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.health = health;
     }
-}
-class IPlayer extends IUser {
     fullName() {
         return `${this.firstName} ${this.lastName}`;
     }
     sayHi(name) {
-        return this.fullName();
+        return `${name} hello how are you!! ${this.fullName()}`;
     }
 }
+const playerA = {
+    name: "taeeon",
+    lastName: "taeeon2",
+};
+const playerB = {
+    name: "taeeon",
+    lastName: "taeeon2",
+    health: 32,
+};
+class UserA {
+    constructor(firstName) {
+        this.firstName = firstName;
+    }
+}
+// * typescript community에서는 클래스나 오브젝트의 shape를 define 하고 싶으면
+// * interface를 사용하고 다른 모든경우에서는 type을 쓰라고 권장한다.
---
+++ src/allCombine.ts
@@ -0,0 +1,30 @@
+interface NewStorage<T> {
+  [key: string]: T;
+}
+
+class LocalStorage<T> {
+  private storage: NewStorage<T> = {};
+  set(key: string, value: T) {
+    this.storage[key] = value;
+  }
+  remove(key: string) {
+    delete this.storage[key];
+  }
+  get(key: string): T {
+    return this.storage[key];
+  }
+  clear() {
+    this.storage = {};
+  }
+}
+
+const stringsStorage = new LocalStorage<string>();
+
+stringsStorage.get("key");
+stringsStorage.set("hello", "test is best");
+console.log(stringsStorage.get("hello"));
+const booleanStorage = new LocalStorage<boolean>();
+
+booleanStorage.get("boolean");
+booleanStorage.set("hello", false);
+console.log(booleanStorage.get("hello"));
