0c049a69b03d6e35e654a307a34caf264ea833de
이태언 <mktears0609@gmail.com>
2023-01-19 22:43:35 +0900

    ADD: typescript config && declare && block class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
-const hello = () => "hi";
+import crypto from "crypto";
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
 
-const test = hello();
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+  }
+}
+
+//*  allow js config가 되어있으면 typescript와 javascript를 같이 사용할 수 있다.
